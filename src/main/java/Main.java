b795167d39ebdf7d2de0a4fcd150eac5497e4ce7
Dmitry Ushakov <ushakov-dp@mail.ru>
2022-06-10 15:08:08 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        var person =  Person.builder();
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,10 @@
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class Person {
+    private  String name;
+}
